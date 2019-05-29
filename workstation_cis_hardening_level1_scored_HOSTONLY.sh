7eee09dd55e9e8afcf2110c8271466bdf8b14e05
Steve Bartlett <50664843+ayethatsright@users.noreply.github.com>
2019-05-29 13:55:43 +0100

    Update workstation_cis_hardening_level1_scored_HOSTONLY.sh

--- workstation_cis_hardening_level1_scored_HOSTONLY.sh
+++ workstation_cis_hardening_level1_scored_HOSTONLY.sh
@@ -3,3 +3,14 @@
 # This script sets the network parameter controls that are needed if the system is only acting as a host.
 # It only includes the two controls not included in the main workstation_cis_hardening_level1_scored.sh script
 
+#########################################################################################################################################
+
+# 3.1.1 Ensure IP forwarding is disabled (Scored)
+
+
+# 3.1.2 Ensure packet redirect sending is disabled (Scored)
+
+# This are only required if the system is to act as a host only.  If needed, run the workstation_cis_hardening_level1_scored_HOSTONLY.sh script to apply these controls
+
+#########################################################################################################################################
+
