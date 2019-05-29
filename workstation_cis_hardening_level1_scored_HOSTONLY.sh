f08c77f33b241ab5df844c5b268552ede43ba8f6
Steve Bartlett <50664843+ayethatsright@users.noreply.github.com>
2019-05-29 13:54:44 +0100

    Create workstation_cis_hardening_level1_scored_HOSTONLY.sh

---
+++ workstation_cis_hardening_level1_scored_HOSTONLY.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# This script sets the network parameter controls that are needed if the system is only acting as a host.
+# It only includes the two controls not included in the main workstation_cis_hardening_level1_scored.sh script
+
