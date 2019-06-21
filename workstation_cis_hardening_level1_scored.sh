5b07f77d6f37b153d171935a84a49a72641d3b1e
Steve Bartlett <50664843+ayethatsright@users.noreply.github.com>
2019-06-21 15:03:03 +0100

    Update workstation_cis_hardening_level1_scored.sh

--- workstation_cis_hardening_level1_scored.sh
+++ workstation_cis_hardening_level1_scored.sh
@@ -1420,7 +1420,10 @@ apt autoremove
 # Setting the desktop background to AFSL one requested by Jamie O'Connor
 
 wget https://cdn-images-1.medium.com/max/1200/1*Y0UYuGcFGSCfs5Eexafq6A.png
-gsettings set org.gnome.desktop.background picture-uri file://./1*Y0UYuGcFGSCfs5Eexafq6A.png
+mkdir /home/share
+mv 1*Y0UYuGcFGSCfs5Eexafq6A.png /home/share/wallpaper.png
+
+su - "$USER" -c "gsettings set org.gnome.desktop.background picture-uri file:///home/share/wallpaper.png"
 
 read -r -p "[i] System will now reboot to ensure all changes take effect. Press ENTER to continue..."
 
